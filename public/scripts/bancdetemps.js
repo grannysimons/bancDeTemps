da6e3326dcc14a68064b768a569ad511332c9bfb
Albert Farre <afarre@sicteltelecom.com>
2018-08-16 20:30:07 +0200

    index.hbs modificaction

--- public/scripts/bancdetemps.js
+++ public/scripts/bancdetemps.js
@@ -1,27 +1,27 @@
-var viewportHeight = $(window).height();  
-var viewportWidth = $(window).width();
+// var viewportHeight = $(window).height();  
+// var viewportWidth = $(window).width();
 
-var heightNavBar = document.getElementById('navbar-main').offsetHeight;
-console.log('La altura del icono menu es de:',heightNavBar);
+// var heightNavBar = document.getElementById('navbar-main').offsetHeight;
+// console.log('La altura del icono menu es de:',heightNavBar);
 
-var heightMainTitle = viewportHeight-2*heightNavBar;
-document.getElementById('text-banc').setAttribute("style",`height:${heightMainTitle}px`);
-document.getElementById('footer-main-page').setAttribute("style",`height:${heightNavBar}px`);
-
-
-// we force the scroll move to top when page is reload
-window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
-}
+// var heightMainTitle = viewportHeight-2*heightNavBar;
+// document.getElementById('text-banc').setAttribute("style",`height:${heightMainTitle}px`);
+// document.getElementById('footer-main-page').setAttribute("style",`height:${heightNavBar}px`);
 
 
-$( window ).resize(() => {
-  viewportHeight = $(window).height(); 
-  heightMainTitle = viewportHeight-2*heightNavBar;
-  // main section index.hbs
-  document.getElementById('text-banc').setAttribute("style",`height:${heightMainTitle}px`);
-  document.getElementById('footer-main-page').setAttribute("style",`height:${heightNavBar}px`);
- });
+// // we force the scroll move to top when page is reload
+// window.onbeforeunload = function () {
+//   window.scrollTo(0, 0);
+// }
+
+
+// $( window ).resize(() => {
+//   viewportHeight = $(window).height(); 
+//   heightMainTitle = viewportHeight-2*heightNavBar;
+//   // main section index.hbs
+//   document.getElementById('text-banc').setAttribute("style",`height:${heightMainTitle}px`);
+//   document.getElementById('footer-main-page').setAttribute("style",`height:${heightNavBar}px`);
+//  });
 
 
 var linkLogin = document.getElementById("my-login");
